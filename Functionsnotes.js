447965450e27f4571c452a02a41b5a4c09f4805b
fy48 <fy48@fresnoU.org>
2023-03-14 10:08:01 -0700

    chapter 2 notes

---
+++ Functionsnotes.js
@@ -0,0 +1,38 @@
+//Functions Notes
+// //Step 1: Define the function
+// function sayHello(){
+//     console.log('Hello');
+// }
+// //Step 2: Call (or use) the function
+// sayHello();
+
+// function printMax(){
+//     let x= 6;
+//     let y= 29;
+//     if (x > y){
+//         console.log(`The larger number is ${x}.`)
+//     } else {
+//         console.log(`The larger number is ${y}.`)
+//     }
+// }
+//
+// printMax();
+
+// function printMax(x, y){
+//     if (x > y){
+//         console.log(`The larger number is ${x}.`)
+//     } else {
+//         console.log(`The larger number is ${y}.`)
+//     }
+// }
+//
+// printMax(16, 9)
+// printMax(2, 75)
+// printMax(2, 2)
+
+function sum(num1, num2){
+    console.log(`${num1} + ${num2} = ${num1} + ${num2}`)
+}
+
+sum(7, 19)
+sum(27, 1)
