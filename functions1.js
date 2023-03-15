d9e9090fff09bb686095e35b1e9b7f976892d3e0
fy48 <fy48@fresnoU.org>
2023-03-15 10:30:13 -0700

    chapter 3 practice

---
+++ functions1.js
@@ -0,0 +1,29 @@
+// function findAreaOfRectangle(num1, num2){
+//     console.log(`${num1} * ${num2} = ${num1 * num2}`)
+// }
+//
+// findAreaOfRectangle(3, 2)
+//
+// function findAreaOfCircle(radius , pi){
+//     console.log(`${radius} * ${pi} = ${radius  * pi}`)
+// }
+//
+// findAreaOfCircle(3, 3.14)
+//
+// function findMin(x, y){
+//     if (x < y){
+//         console.log(`The smaller number is ${x}.`)
+//     } else {
+//         console.log(`The smaller number is ${y}.`)
+//     }
+// }
+// findMin(10, 48)
+
+function checkEquality (x, y){
+    if (x == y){
+        console.log('The number is equal.')
+    } else {
+        console.log('They are not equal.')
+    }
+}
+checkEquality (11, 48)
