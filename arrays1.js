d18856c9cdad24ea4a4a50b8de5011d86dad2e99
fy48 <fy48@fresnoU.org>
2023-03-24 10:24:45 -0700

    array practice

---
+++ arrays1.js
@@ -0,0 +1,52 @@
+//1
+
+// function firstelement(arr){
+// console.log( arr[0] )
+// } firstelement( ['a', 'b', 'c', 'd'] )
+// firstelement( [7, 5, 3, 1] )
+
+//2
+
+// function lastelement(arr){
+// console.log( arr[arr.length - 1] )
+// } lastelement( ['a', 'b', 'c', 'd'] )
+// lastelement( [7, 5, 3, 1] )
+
+//3
+
+// let number = ['5', '6', '7', '8' ];{
+// }
+// console.log(number[number.length=0])
+
+//4
+
+// let number = ['5', '6', '7', '8' ];
+// for (let number = 8; number >= 5 ; number--){
+// console.log(number)
+// }
+
+//5
+
+// let number = ['9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];{
+// }
+// console.log(number[number.length-8])
+// console.log(number[number.length-5])
+// console.log(number[number.length-2])
+
+//6
+
+// let number = ['9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];{
+// }
+// console.log(number[number.length-8])
+// console.log(number[number.length-6])
+// console.log(number[number.length-4])
+// console.log(number[number.length-2])
+
+//7
+
+// let number = ['1', '2', '3', '4', '5', '6', '7', '8','9', '10', '11', '12', '13'];
+// for (let number = 4; number <= 13 ; number++){
+// console.log(number)
+// }
+
+//8
